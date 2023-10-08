f4949ec73466006552c84b37f6b02221c801a700
Yasmintao <yasminhassaniah00@gmail.com>
2023-10-08 11:00:47 +0300

    Initial Commit

---
+++ Yasmin's app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yasmin's app
+//
+//  Created by Yasmin on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yasmin's app/Yasmin_s_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yasmin_s_appApp.swift
+//  Yasmin's app
+//
+//  Created by Yasmin on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Yasmin_s_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
