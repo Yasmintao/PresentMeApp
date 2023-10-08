247ddbde514fed86144491f8c25b882432075fc2
Maryam Mohammad <maryammohammad@10-101-50-171.0200.twq.sa.iosda.org>
2023-10-08 12:42:59 +0300

    maryams 2nd view

---
+++ Yasmin's app/MaryamView.swift
@@ -0,0 +1,18 @@
+//
+//  MaryamView.swift
+//  Yasmin's app
+//
+//  Created by Maryam Mohammad on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct MaryamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaryamView()
+}
