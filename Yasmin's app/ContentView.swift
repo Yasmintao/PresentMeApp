37be9783cafe4916b16371416f552eeb11d8d072
Yasmintao <yasminhassaniah00@gmail.com>
2023-10-08 11:33:22 +0300

    new file

--- Yasmin's app/ContentView.swift
+++ Yasmin's app/ContentView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+        
+        VStack{
+            Text(" ✨ It's Yasmin !✨").font(.largeTitle).fontWeight(.heavy).foregroundColor(Color(hue: 0.582, saturation: 0.891, brightness: 0.795))
+            
+            Image("Yasmin")
+                .resizable()
+                .frame(width: 150, height: 157
+                )
+            
+            Text(" A 23 years old Master student👩🏻‍💻, always have passion for learning and developing my self📚, and my hobby is  enjoying life as much as I can 🫶🏻 ")
+                .font(.body)
+                .fontWeight(.thin)
+                .foregroundColor(Color(hue: 0.639, saturation: 0.935, brightness: 0.667))
+                .multilineTextAlignment(.center)
+                
+            
+          
         }
         .padding()
     }
