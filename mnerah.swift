49a3e0f3785af79cf05857dd8913c7b46ad9d95c
mnerahawn <mnerahawncs@gmail.com>
2023-10-08 12:58:44 +0300

    new

---
+++ mnerah.swift
@@ -0,0 +1,18 @@
+//
+//  mnerah.swift
+//  Yasmin's app
+//
+//  Created by mnerah awn on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct mnerah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mnerah()
+}
