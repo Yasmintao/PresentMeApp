e29caedaafd33f35b0158ddd2417ea7ee8644e2e
Aisha25 <aishaalnozili83@gmail.com>
2023-10-08 12:27:34 +0300

    Aisha's commit

--- Presentmeapp.swift
+++ Presentmeapp.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
-struct Presentmeapp: View {
+struct MeApp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image("Me")
+                .resizable()
+                .frame(width:200, height:200)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+               // .shadow(radius:10)
+                .overlay(Circle().stroke(Color.gray,lineWidth: 5))
+            
+            
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                Text("Aisha Alnozili")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.gray)
+                Text(" 21 yeras old ,IT student and  Matcha lover🍵 ")
+                    .foregroundColor(.brown)
+                
+                
+            })
+            
+            
+            
+        }
     }
 }
 
 #Preview {
-    Presentmeapp()
+    MeApp()
 }
