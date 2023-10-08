5a084560ab63ed2b9b29157272f10c58813d37c3
mnerahawn <mnerahawncs@gmail.com>
2023-10-08 13:03:20 +0300

    new

--- mnerah.swift
+++ mnerah.swift
@@ -4,15 +4,40 @@
 //
 //  Created by mnerah awn on 23/03/1445 AH.
 //
-
 import SwiftUI
 
-struct mnerah: View {
+struct mnerahawn1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+     
+        ZStack{
+            Color.gray
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("mnerahawn")
+                    .resizable()
+                    .frame(width: 200, height:200)
+                    .cornerRadius(50)
+                    .padding(50)
+                
+                
+                
+                VStack(spacing:20) {
+                    Text ("Mnerah Awn Alsubaie")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Text("I am 26 year old ,Computer science graduate from Taif University. UX/UI designer. I love experiments and  i am passionate about developing myself")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                    
+                }
+                
+            }
+        }
     }
+    
 }
 
 #Preview {
-    mnerah()
+    mnerahawn1()
 }
