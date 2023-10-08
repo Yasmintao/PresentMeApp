b83fd58fe21bf3cf409e5520644578515361ce5c
Aisha25 <aishaalnozili83@gmail.com>
2023-10-08 12:24:12 +0300

    Aisha

---
+++ Presentmeapp.swift
@@ -0,0 +1,18 @@
+//
+//  Presentmeapp.swift
+//  Yasmin's app
+//
+//  Created by Aisha Alnozili on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Presentmeapp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Presentmeapp()
+}
